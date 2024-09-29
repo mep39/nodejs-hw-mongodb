564f0c2ce7cf64e06da3e2f968e272c58fb9e4f1
mepinit <evgenmep@gmail.com>
2024-09-29 20:14:13 +0300

    v3.01

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { env } from './utils/env.js';
 
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 
+import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 
 export const startServer = () => {
@@ -13,7 +15,9 @@ export const startServer = () => {
 
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
 
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
   app.use(notFoundHandler);
