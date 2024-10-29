3549e11dee492f6eb1c4b42f44fb631e628da196
mepinit <evgenmep@gmail.com>
2024-10-29 20:43:34 +0200

    5.1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import { UPLOAD_DIR } from './constants/index.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 import { env } from './utils/env.js';
 
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
+import logger from '../src/middlewares/loger.js';
 
 import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
@@ -14,10 +15,11 @@ import contactsRouter from './routers/contacts.js';
 export const startServer = () => {
   const app = express();
 
+  app.use(logger);
   app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
-  // app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
